be5dd073a2502b9f07e9489aedb496ce9f390e03
KarinaBashkatova <mirzoevakarina53@gmail.com>
2022-09-02 12:22:07 +0300

    Changing FetchType to LAZY.

--- PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/repositories/UsersRepository.java
+++ PP_3_1_2_Boot_Security-master/src/main/java/ru/kata/spring/boot_security/demo/repositories/UsersRepository.java
@@ -1,5 +1,6 @@
 package ru.kata.spring.boot_security.demo.repositories;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.model.User;
@@ -13,5 +14,6 @@ import java.util.Optional;
 // Реализация доступа к БД
 @Repository
 public interface UsersRepository extends JpaRepository<User, Integer> {
+    @EntityGraph(value = "graph.User.roles", type = EntityGraph.EntityGraphType.LOAD)
     Optional<User> findByUserName(String username);
 }
